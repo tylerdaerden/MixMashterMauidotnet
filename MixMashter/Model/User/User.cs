add3e7fd28a8b6652f9f1e5bcc6fc38d5b57b267
Tyler Daerden <daerdentyler@gmail.com>
2024-02-15 20:42:15 +0100

    Création Dossiers Structure MVVM + Début Classe User + Tools

---
+++ MixMashter/Model/User/User.cs
@@ -0,0 +1,74 @@
+﻿using MixMashter.ToolBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixMashter.Model.User
+{
+    public class User
+    {
+
+
+        #region Attributs
+
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _userName;
+        private string _email;
+        private DateTime _birthDate;
+        private string _password;
+
+        #endregion
+
+        #region Props
+
+        public int Id { get => _id; set => _id = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public string UserName { get => _userName; set => _userName = value; }
+        public string Email 
+        {
+            get => _email;
+            set
+            {
+                if (Tools.CheckEMail(value))
+                {
+                    _email = value;
+                }
+            } 
+        }
+
+
+        public DateTime BirthDate //ici faire vérification sur âge pour mettre un age minimal de 13 ans 
+        {
+            get => _birthDate;
+            set
+            {
+                //if(//insérer ici méthode de vérificaion date)
+                {
+                    _birthDate = value;
+                }
+
+            }
+        }
+        public string Password { get => _password; set => _password = value; } 
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MixMashter/ToolBox/Tools.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MixMashter.ToolBox
+{
+    public class Tools
+    {
+
+        public static bool CheckEntryName(string name)
+        {
+            //name = "P- Henri";
+            //si le nom débute ou termine par un espace ou un tiret
+            if (name.StartsWith(" ") || name.StartsWith("-") || name.EndsWith(" ") || name.EndsWith("-"))
+            {
+                //MessageBox.Show($"La saisie {name} ne peut commencer ou se terminer par un espace ou un tiret", "Erreur de saisie", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            //si le nom contient au moins un double espace.
+            if (name.Contains(" ") || name.Contains("--"))
+            {
+                //MessageBox.Show($"La saisie {name} comporte au moins un double espace ou tiret non autorisé", "Erreur de saisie", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            //test s'il y a présence de caractère spéciaux (en dehors de l'alphabet) ou accentués sans tenir compte d'un espace ou un tiret (derrière le Z: Z -)
+            if (!Regex.IsMatch(name, @"^[a-zA-Z -]*$"))
+            {
+                //MessageBox.Show($"La saisie {name} ne peut comporter de caractères spéciaux ou accentués", "Erreur de saisie", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            string[] nameParts = name.Split('-', ' ');
+            foreach (string s in nameParts)
+            {
+                //test si la première lettre est une majuscule et les suivantes de a à z en minuscule et sans accent
+                if (!Regex.IsMatch(s, @"^[A-Z][a-z]*$"))
+                {
+                    //MessageBox.Show($"La saisie {name} ne correpond pas aux critères. La première lettre de chaque élément qui compose la saisie doit être une majuscule et les suivantes des minuscules.", $"Erreur de saisie {name}", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return false;
+                }
+            }
+            //tous les tests concluants, saisie valide.
+            return true;
+        }
+
+        public static bool CheckEMail(string tryEmail)
+        {
+            /*format accepté semblable à "a@b.com";
+            obligatoirement de 1 à 25 caractères de a z, 0 à 9 puis obligatoirement le @ puis
+            obligatoirement de 1 à 20 caractère de a z, 0 à 9 puis un . puis 2 à 3 caractères (be, com, fr, ...*/
+            if (!string.IsNullOrEmpty(tryEmail))
+            {
+                if (!Regex.IsMatch(tryEmail, @"^[a-z0-9]{1,25}@[a-z0-9]{1,20}\.[a-z]{2,3}$"))
+                {
+                    //MessageBox.Show($"L'adresse mail est incorrecte", "Erreur de saisie", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
