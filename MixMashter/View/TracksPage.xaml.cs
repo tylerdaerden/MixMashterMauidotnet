fd62903d681320732aa488c1310b110e7f05f4b0
Tyler_Daerden <daerdentyler@gmail.com>
2024-05-15 22:12:24 +0200

    Transfert MainPage → Tracks Page et restored HomePage

--- MixMashter/View/TracksPage.xaml.cs
+++ MixMashter/View/TracksPage.xaml.cs
@@ -1,12 +1,39 @@
+using MixMashter.Utilities.Interfaces;
 using MixMashter.ViewModel;
 
 namespace MixMashter.View;
 
 public partial class TracksPage : ContentPage
 {
-	public TracksPage(TracksPageViewModel tracksPageVM)
+	public TracksPage(TracksPageViewModel tracksPageVM, IDataAccess dataAcessService, IAlertService alertService)
 	{
-		BindingContext = tracksPageVM;
+        dataAccess = dataAcessService;
+        alert = alertService;
+        tracksPageViewModel = tracksPageVM;
+        // Definition du BindingContext avec le ViewModel
+        BindingContext = tracksPageVM;
 		InitializeComponent();
 	}
+
+    #region Props
+
+    /// <summary>
+    /// Manager to the data access (CSV , JSON , SQL ) 
+    /// </summary>
+    private IDataAccess dataAccess;
+
+    /// <summary>
+    /// Manager to the data access (CSV , JSON , SQL ) 
+    /// </summary>
+    private IAlertService alert;
+
+    /// <summary>
+    /// keep a reference to the ViewModel for eventual testings
+    /// </summary>
+    private TracksPageViewModel tracksPageViewModel;
+
+
+
+    #endregion
+
 }
--- MixMashter/ViewModel/TracksPageViewModel.cs
+++ MixMashter/ViewModel/TracksPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MixMashter.Utilities.Interfaces;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MixMashter.Model.Tracks;
+using MixMashter.Utilities.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +10,63 @@ using System.Threading.Tasks;
 
 namespace MixMashter.ViewModel
 {
-    public class TracksPageViewModel : BaseViewModel
+    public partial class TracksPageViewModel : BaseViewModel
     {
-        public TracksPageViewModel(IAlertService alertService) : base(alertService)
+
+        #region Constructeurs
+
+        public TracksPageViewModel(IDataAccess dataAccessService, IAlertService alertService) : base(alertService)
         {
+            dataAcess = dataAccessService;
+            Trackslist = dataAccessService.GetAllTracks(); // get all tracks  from chosen DataAccessSource
+        } 
+
+        #endregion
+
+        #region Props
+        /// <summary>
+        /// manager to the data access (Csv, Json , XAML , SQL) etc... 
+        /// </summary>
+        private IDataAccess dataAcess;
+
+        /// <summary>
+        /// Collection of all tracks
+        /// </summary>
+        public TracksCollection Trackslist { get; set; }
+
+        [ObservableProperty]
+        private Tracks tracksUserSelection;
 
 
-
+        [RelayCommand()]
+        private async void ShowTracksDetails()
+        {
+            if (TracksUserSelection != null)
+            {
+                await alertService.ShowAlert("Selection", $"Votre Choix :\n{TracksUserSelection.Name}\n" +
+                    $"{TracksUserSelection.ArtistName}\n{TracksUserSelection.Length}sec\nlien de lecture YTB : {TracksUserSelection.Urlpath} ");
+            }
+            else
+            {
+                await alertService.ShowAlert("Erreur", "Aucune Piste n'a été choisie");
+            }
         }
+
+        [RelayCommand()]
+        private async void TestBindingShowProperties()
+        {
+            await alertService.ShowAlert("Informations de l'application ", $"En interne, les valeur des propriétés sont : " +
+            $"\n{MainInfos.Name}\n{MainInfos.WebSite}\n");
+        }
+        [RelayCommand()]
+        private async void TestBindingChangeProperties()
+        {
+            MainInfos.Name = "MixMashterPREMIUM";
+            MainInfos.WebSite = "http://MixMashterPremium.com";
+        }
+
+
+        #endregion
 
     }
 }
